570baa70e61e35e7f69e7c9582f694808b3beacc
Abhinay Pratap <abhinaypratap@icloud.com>
2021-01-06 22:29:40 +0530

    modified Stack class

---
+++ Stack.java
@@ -0,0 +1,16 @@
+
+public class Stack {
+	private int[] sArray;
+	private int top;
+
+	public Stack(int size) {
+		sArray = new int[size];
+		top = -1;
+	}
+
+	public void push(int value) {};
+	public int pop() {};
+	public int peek() {};
+	public boolean isEmpty() {};
+	public boolean isFull() {};
+}
